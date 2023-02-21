54e97e3f9e9e827a4e35bea9699fd361d32f53e9
sghajdao <saadghajdaoui@gmail.com>
2023-02-21 14:35:56 +0000

    wordpress

--- srcs/requirements/mariaDB/tools/setup.sh
+++ srcs/requirements/mariaDB/tools/setup.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
 service mysql start
-
 
 echo "CREATE USER IF NOT EXISTS $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON * . * TO $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
@@ -9,9 +7,3 @@ echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root -p$MYSQL_ROOT_PASSWORD
-
-# echo "CREATE IF NOT EXISTS DATABASE ${MYSQL_DATABASE};" | mysql -u root
-# echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql -u root
-# echo "GRANT ALL PRIVILEGES ON ${MYSQL_USER}.* TO '${MYSQL_USER}'@'%';" | mysql -u root
-# echo "FLUSH PRIVILEGES;" | mysql -u root
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql -u root
